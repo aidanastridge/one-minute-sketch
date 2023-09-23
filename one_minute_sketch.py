29c053416acfb6810d5f7e98a0552efb9fedf2e5
Aidan Astridge <115724380+aidanastridge@users.noreply.github.com>
2023-09-23 10:06:21 -0400

    Update one_minute_sketch.py

--- one_minute_sketch.py
+++ one_minute_sketch.py
@@ -1,16 +1,47 @@
-import cv2
-import random
-
 #This script as of right now only captures a random frame out of a downloaded video.
 #I will be creating an app that can take a video from youtube that shows a random frame for a minute.
 #Intended for electronic music videos
 
-vidcap = cv2.VideoCapture('video.mp4')
-# get total number of frames
-totalFrames = vidcap.get(cv2.CAP_PROP_FRAME_COUNT)
-randomFrameNumber=random.randint(0, totalFrames)
-# set frame position
-vidcap.set(cv2.CAP_PROP_POS_FRAMES,randomFrameNumber)
-success, image = vidcap.read()
-if success:
-    cv2.imwrite("random_frame.jpg", image)
+import os
+import random
+import cv2
+from pytube import YouTube
+
+# Replace with the URL of the YouTube video you want to capture a frame from
+video_url = "https://www.youtube.com/"
+
+# Define the output directory where the video will be downloaded
+output_dir = "downloaded_videos"
+
+# Create the output directory if it doesn't exist
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# Download the YouTube video
+yt = YouTube(video_url)
+video_stream = yt.streams.filter(file_extension="mp4", res="720p").first()
+video_filename = os.path.join(output_dir, yt.title + ".mp4")
+video_stream.download(output_path=output_dir)
+
+# Open the downloaded video and capture a random frame
+cap = cv2.VideoCapture(video_filename)
+frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+random_frame_number = random.randint(0, frame_count - 1)
+
+# Set the frame number to the random frame
+cap.set(cv2.CAP_PROP_POS_FRAMES, random_frame_number)
+
+# Read the frame
+ret, frame = cap.read()
+
+if ret:
+    # Save the random frame as an image
+    random_frame_filename = os.path.join(output_dir, "random_frame.png")
+    cv2.imwrite(random_frame_filename, frame)
+    print(f"Random frame saved as {random_frame_filename}")
+else:
+    print("Error capturing random frame")
+
+# Release the video capture and close the window
+cap.release()
+cv2.destroyAllWindows()
